c8b73b69d97800333f12a9ec714f19b236056eed
nikhilkumarchauhan165@gmail.com <nikhilkumarchauhan165@gmail.com>
2025-07-06 16:58:13 +0530

    v2

---
+++ src/app/performance/page.jsx
@@ -0,0 +1,9 @@
+export default function PerformancePage() {
+  return (
+    <div>
+      <h1 className=" text-2xl center mt-8">Performance Page</h1>
+      <p>This page is designed to showcase performance optimizations.</p>
+      {/* Add more content or components as needed */}
+    </div>
+  );
+}
---
+++ src/header/Header.jsx
@@ -0,0 +1,9 @@
+
+const Header = () => {
+ 
+  return (
+    <h1>wecom</h1>
+  )
+}
+
+export default Header
